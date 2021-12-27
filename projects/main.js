5cf7e4187174b28f1761ae03c8156e28b44bc3e5
QuentinSlagter <93204007+QuentinSlagter@users.noreply.github.com>
2021-12-27 14:56:44 -0600

    new updates

--- projects/main.js
+++ projects/main.js
@@ -1,46 +1,3 @@
-// Dropdown menu in navigation bar
-document.addEventListener("click", e => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]")
-  if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
-
-  let currentDropdown
-    if (isDropdownButton) {
-      currentDropdown = e.target.closest('[data-dropdown]')
-      currentDropdown.classList.toggle('active')
-    }
-  document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
-    if (dropdown === currentDropdown) return
-    dropdown.classList.remove('active')
-  })
-})
-// End of dropdown menu
-
-// Navigation bar to scroll through the home page
-$(document).ready(function(){
-
-	$("nav a").on("click", function(event){
-		event.preventDefault();
-		$("nav").addClass("fixed");
-		id = ($(this).attr("href"));
-		scrollVertical = $(id).offset().top;
-
-		$("body, html").animate({scrollTop: scrollVertical});
-	});
-
-	$(document).on("scroll", function(){
-		secondPage = $("nav li:nth-child(2) a").attr("href");
-
-		if ( $("body").scrollTop() >= $("nav").height() )
-		{
-			$("nav").addClass("fixed");
-		} else {
-			$("nav").removeClass("fixed");
-		}
-	});
-});
-// End of navigation bar
-
-// Contact form
 const contactForm = document.getElementById('contactForm');
 const contactButton = document.getElementById('contactButton');
 const form = document.getElementsByClassName('form');
@@ -133,4 +90,3 @@ function isMobileNumberValid(mobileNumber) {
 function isEmailValid(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
-// End of contact form
