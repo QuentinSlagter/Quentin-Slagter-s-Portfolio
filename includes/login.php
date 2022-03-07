1798793d0122f3c4da7bfdcb3fddff9c76d28c62
QuentinSlagter <93204007+QuentinSlagter@users.noreply.github.com>
2022-03-07 15:54:27 -0600

    updated Sessions settings for blog

--- includes/login.php
+++ includes/login.php
@@ -1,14 +1,9 @@
 <?php ob_start(); ?>
-<?php include "db.php"; ?>
 <?php session_start(); ?>
 
 <?php 
 
 if(isset($_POST['login'])) {
-  $_SESSION['username'] = 'Admin Guest';
-  $_SESSION['firstName'] = $db_user_firstName;
-  $_SESSION['lastName'] = $db_user_lastName;
-  $_SESSION['user_role'] = 'admin';
 
   header("Location: https://power-of-programming.herokuapp.com/admin/admin_index.php");
 }
--- index.php
+++ index.php
@@ -4,12 +4,9 @@
 
 <?php 
 
-if(isset($_POST['login'])) {
   $_SESSION['username'] = 'Admin Guest';
-  $_SESSION['firstName'] = $db_user_firstName;
-  $_SESSION['lastName'] = $db_user_lastName;
   $_SESSION['user_role'] = 'admin';
-}
+
 
 ?>
 
